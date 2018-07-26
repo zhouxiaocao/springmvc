5a1186ba5bb36c25fe19f7c2018e7bb75a393ff0
zhou zhihu <zhouzh@gegejia.com>
2018-07-26 08:56:31 +0800

    初始化项目

---
+++ src/main/java/com/zzh/mvc/MvcApplication.java
@@ -0,0 +1,14 @@
+package com.zzh.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcApplication
+{
+    
+    public static void main(String[] args)
+    {
+        SpringApplication.run(MvcApplication.class, args);
+    }
+}
---
+++ src/test/java/com/zzh/mvc/MvcApplicationTests.java
@@ -0,0 +1,18 @@
+package com.zzh.mvc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MvcApplicationTests
+{
+    
+    @Test
+    public void contextLoads()
+    {
+    }
+    
+}
